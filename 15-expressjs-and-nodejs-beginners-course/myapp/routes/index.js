1cd738f63bcaf5467d77c12a4af30e8ec2b8262d
Deepak Dargade <deepakdargade@gmail.com>
2022-03-15 21:39:40 +0530

    landing page added

--- 15-expressjs-and-nodejs-beginners-course/myapp/routes/index.js
+++ 15-expressjs-and-nodejs-beginners-course/myapp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-let index = require('../controllers/index');
+let landing = require('../controllers/landing');
 
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
 
 module.exports = router;
